3d9826d2053b883f8b113e3acc1fb1ec4b1da2ae
Kajkusemil <kajkusemil2103@gmail.com>
2025-07-01 23:05:58 +0200

    Update etl_srbija.py

--- .github/workflows/etl_srbija.py
+++ .github/workflows/etl_srbija.py
@@ -2,5 +2,4 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
 df = spark.read.csv("/databricks-datasets/samples/population-vs-price/data_geo.csv", header=True)
-df.show()
-#Test komentar
+df.count()
